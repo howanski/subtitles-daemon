1267051a64f776966393d81ffd02a605e2dac0b5
Maciej Howański <maciej.howanski@outlook.com>
2022-03-05 20:06:21 +0100

    feat: add datetime info to log

--- docker/subtitles-daemon.sh
+++ docker/subtitles-daemon.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 while :
 do
+  echo "Running subtitles fetch..."
   subliminal --opensubtitles $OPENSUBTITLES_USER $OPENSUBTITLES_API_KEY download /videos -l $OPENSUBTITLES_LANG
   chown -R $USER:$USER /videos
+  echo "-------------"
+  date 
+  echo "Sleeping for $OPENSUBTITLES_FREQUENCY seconds..."
+  echo "-------------"
   sleep $OPENSUBTITLES_FREQUENCY
 done
