10c8d21585097a1e80b02f262d547aa351ef023f
Maciej Howański <maciej.howanski@outlook.com>
2022-03-06 19:44:36 +0100

    feat: added support for addic7ed and omdb

--- docker/subtitles-daemon.sh
+++ docker/subtitles-daemon.sh
@@ -2,11 +2,11 @@
 while :
 do
   echo "Running subtitles fetch..."
-  subliminal --opensubtitles $OPENSUBTITLES_USER $OPENSUBTITLES_API_KEY download /videos -l $OPENSUBTITLES_LANG
-  chown -R $USER:$USER /videos
+  subliminal --opensubtitles $OPENSUBTITLES_USER $OPENSUBTITLES_PASSWORD --omdb $OMDB_API_KEY --addic7ed $ADDIC7ED_USER $ADDIC7ED_PASSWORD download /videos -l $FETCH_LANG
+  chown -R $FILESYSTEM_USER:$FILESYSTEM_USER /videos
   echo "-------------"
   date 
-  echo "Sleeping for $OPENSUBTITLES_FREQUENCY seconds..."
+  echo "Sleeping for $FETCH_FREQUENCY seconds..."
   echo "-------------"
-  sleep $OPENSUBTITLES_FREQUENCY
+  sleep $FETCH_FREQUENCY
 done
